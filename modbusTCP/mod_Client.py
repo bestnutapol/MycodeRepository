2e685d9f5b9dbb5ee2dbf24eeba8c7f69fbf25b6
BestNuttapol <nuttapol.pomtong@gmail.com>
2021-07-07 04:52:49 +0700

    Add files via upload

--- modbusTCP/mod_Client.py
+++ modbusTCP/mod_Client.py
@@ -1,11 +1,24 @@
 import time
 from pymodbus.client.sync import ModbusTcpClient
+import datetime
 IP_Address = 'localhost'
 client = ModbusTcpClient(IP_Address)
 print(client.connect())
+#client.write_coil(0,True)
 #client.write_registers(0,50)
+address_count = 10
+old = []
+for i in range(address_count): old.append('')
 while True:
-    result = client.read_holding_registers(0,1,unit=0x01)
-    print(result.registers[0])
-    time.sleep(5)
+    x = datetime.datetime.now()
+    result = client.read_holding_registers(0,address_count,unit=0x01)
+
+    h = result.registers
+    for j in range(address_count):
+        if old[j] != h[j]:
+            if old[j] != '':
+                print( 'Time : '+ str(x) +' Address : '+ str(j) +' Value : '+ str(h[j]))
+            old[j] = h[j]
+
+    time.sleep(0.1)
 client.close()
